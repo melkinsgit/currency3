57527745f83ec2aead51d4d6f8fa142867a1343b
melkinsgit <margaret.elkins@gmail.com>
2016-03-04 18:51:52 -0600

    Lab 07 requirement

--- routes/index.js
+++ routes/index.js
@@ -13,19 +13,54 @@ function homepage (req, res) {
 router.get('/convert', convert);
 
 function convert(req, res) {
-	var dollars = req.query.dollarAmount;
+	var amountToConvert = req.query.convertAmount;
+	var convertFrom = req.query.fromCurrency;
 	var convertTo = req.query.toCurrency;
+	var convertedVal;
 
-	console.log("query was: convert " + dollars + " to " + convertTo);
+	//console.log("query was: convert " + dollars + " to " + convertTo);
 
-	var conversions = {"Pound":1.6, "Euro":1.1};
+	var dollarConversions = {"Pounds": 0.7, "Euros": 0.9};
+	var poundToEuro = 1.3;
 
-	var conversionRate = conversions[convertTo];
+	// give user for dropdown choice that doesn't convert - the same currency in each
+	if (convertFrom === convertTo) {
+	}
+	else {
+		// get the currency conversion factor
+		if (convertFrom === 'Dollars') {
+			var conversionRate = dollarConversions[convertTo];
+			convertedVal = amountToConvert * conversionRate;
+		}
 
-	var convertedVal = conversionRate * dollars;
+		if (convertFrom === 'Euros') {
+			if (convertTo === 'Dollars') {
+				conversionRate = dollarConversions[convertFrom];
+				convertedVal = amountToConvert / conversionRate;
+			}
+			if (convertTo === 'Pounds') {
+				conversionRate = poundToEuro;
+				convertedVal = amountToConvert / conversionRate;
+			}
+		}
 
-	res.render('result', {dollars:dollars, currency:convertTo, converted:convertedVal});
-
+		if (convertFrom === 'Pounds') {
+			if (convertTo === 'Dollars') {
+				conversionRate = dollarConversions[convertFrom];
+				convertedVal = amountToConvert / conversionRate;
+			}
+			if (convertTo === 'Euros') {
+				conversionRate = poundToEuro;
+				convertedVal = amountToConvert * conversionRate;
+			}
+		}
+		res.render('result', {
+			amount: amountToConvert,
+			frCurrency: convertFrom,
+			destCurrency: convertTo,
+			toConverted: convertedVal
+		});
+	}
 }
 
 module.exports = router;
